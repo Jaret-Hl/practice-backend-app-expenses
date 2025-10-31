5afdf2f7c0d4196d1b10db9972add44ae4e0b630
Jaret <104955952+Jaret-Hl@users.noreply.github.com>
2025-10-31 16:33:11 -0600

    Refactor tenant controller to improve query handling and code consistency

--- src/controllers/tenants.controller.js
+++ src/controllers/tenants.controller.js
@@ -1,21 +1,36 @@
 // src/controllers/tenants.controller.js
-import { supabase } from '../db.js'
+import { supabase } from "../db.js";
 
 export const getTenants = async (req, res) => {
-  const { data, error } = await supabase.from('tenant').select('*')
-  if (error) return res.status(500).json({ error: error.message })
-  res.json(data)
-}
+  //1. capturar el parametro de consulta "active"
+  const { is_active } = req.query;
+  // 2. inicializar la consulta
+  let query = supabase.from("tenant").select("*");
+  // 3. aplicar filtro condicionalmente
+  if (is_active !== undefined) {
+    const isActiveBool = is_active === "true";
+    query = query.eq("is_active", isActiveBool);
+  }
+  const { data, error } = await query;
+
+  if (error) return res.status(500).json({ error: error.message });
+  res.json(data);
+};
 
 export const getTenantById = async (req, res) => {
   const { id } = req.params;
-  const { data, error } = await supabase.from('tenant').select('*').eq('id', id).single()
+  const { data, error } = await supabase
+    .from("tenant")
+    .select("*")
+    .eq("id", id)
+    .single();
 
-  if (error && error.code !== 'PGRST116') return res.status(500).json({ error: error.message })
-  if (!data) return res.status(404).json({ error: 'No se encontró el tenant' })
+  if (error && error.code !== "PGRST116")
+    return res.status(500).json({ error: error.message });
+  if (!data) return res.status(404).json({ error: "No se encontró el tenant" });
 
-  res.json(data)
-}
+  res.json(data);
+};
 
 /* export const createTenant = async (req, res) => {
   const { title, category, amount, date, userId } = req.body;
@@ -36,23 +51,25 @@ export const updateTenant = async (req, res) => {
   const { id } = req.params;
   const updatedTenant = req.body;
   const { data, error } = await supabase
-    .from('tenant')
+    .from("tenant")
     .update(updatedTenant)
-    .eq('id', id)
+    .eq("id", id)
     .select();
 
-  if (error) return res.status(500).json({ error: 'No se pudo actualizar el tenant' });
+  if (error)
+    return res.status(500).json({ error: "No se pudo actualizar el tenant" });
   res.json(data[0]);
-}
+};
 
 export const deleteTenant = async (req, res) => {
   const { id } = req.params;
   const { data, error } = await supabase
-    .from('tenant')
+    .from("tenant")
     .delete()
-    .eq('id', id)
+    .eq("id", id)
     .select();
 
-  if (error) return res.status(500).json({ error: 'No se pudo eliminar el tenant' });
+  if (error)
+    return res.status(500).json({ error: "No se pudo eliminar el tenant" });
   res.json({ message: `Tenant with ID: ${id} deleted` });
-}
+};
