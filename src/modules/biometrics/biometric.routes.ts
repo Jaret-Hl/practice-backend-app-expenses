36ef2a8c9aee58f814a3b81a2b1ca3b78d20d582
Jaret-Hl <gadir041499@gmail.com>
2026-05-29 14:58:55 -0600

    refactor: Update biometric routes and controller for improved authorization and middleware integration

--- src/modules/biometrics/biometric.routes.ts
+++ src/modules/biometrics/biometric.routes.ts
@@ -1,18 +1,57 @@
 import { Router } from "express";
-import { getBiometrics, getBiometricById, updateBiometric, deleteBiometric, createBiometric } from "../biometrics/biometrics.controller.js";
-import { authMiddleware } from "../../shared/middlewares/auth.middleware.js";
+import {
+  getBiometrics,
+  getBiometricById,
+  updateBiometric,
+  deleteBiometric,
+  createBiometric,
+} from "../biometrics/biometrics.controller.js";
+import { authenticateJWT } from "../../shared/middlewares/auth.middleware.js";
 import { validate } from "../../shared/middlewares/validate.middleware.js";
-import { BiometricCreateSchema, BiometricUpdateSchema } from "./biometrics.schema.js";
+import {
+  BiometricCreateSchema,
+  BiometricUpdateSchema,
+} from "./biometrics.schema.js";
+import { authorize } from "../../shared/middlewares/authorization.middleware.js";
+import { PERMISSIONS } from "../../shared/constants/permissions.js";
 
 const router = Router();
 
-router.get("/biometrics", authMiddleware, getBiometrics);
+router.get(
+  "/biometrics",
+  authenticateJWT,
+  authorize([PERMISSIONS.BIOMETRIC_READ]),
+  getBiometrics,
+);
 
-router.get("/biometrics/:id", authMiddleware, getBiometricById);
+router.get(
+  "/biometrics/:id",
+  authenticateJWT,
+  authorize([PERMISSIONS.BIOMETRIC_READ]),
+  getBiometricById,
+);
 
-router.post("/biometrics", authMiddleware, validate(BiometricCreateSchema), createBiometric);
+router.post(
+  "/biometrics",
+  authenticateJWT,
+  authorize([PERMISSIONS.BIOMETRIC_CREATE]),
+  validate(BiometricCreateSchema),
+  createBiometric,
+);
 
-router.put("/biometrics/:id", authMiddleware, validate(BiometricUpdateSchema), updateBiometric);
+router.put(
+  "/biometrics/:id",
+  authenticateJWT,
+  authorize([PERMISSIONS.BIOMETRIC_UPDATE]),
+  validate(BiometricUpdateSchema),
+  updateBiometric,
+);
 
-router.delete("/biometrics/:id", authMiddleware, deleteBiometric);
+router.delete(
+  "/biometrics/:id",
+  authenticateJWT,
+  authorize([PERMISSIONS.BIOMETRIC_DELETE]),
+  deleteBiometric,
+);
+
 export default router;
